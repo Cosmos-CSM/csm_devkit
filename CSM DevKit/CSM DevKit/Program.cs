b7ae69be21815befe1eee78dd93c6463bf1df27b
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2026-07-27 04:37:26 -0700

    adding configs

---
+++ CSM DevKit/CSM DevKit/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.CommandLine;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        Command dbCommands = new(
+                "db",
+                "Database related commands"
+            );
+
+        dbCommands.SetAction(
+                (parseResult) =>
+                {
+                    Console.WriteLine("Welcome to database related commands");
+                    return 0;
+                }
+            );
+        
+        
+        RootCommand rootCommand = new("csmdk")
+        {
+            Subcommands = {
+                    dbCommands
+                }
+        };
+
+
+        return rootCommand.Parse(args).Invoke();
+    }
+}
